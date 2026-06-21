1e323f9e087ca8e8dd30c572fbd203e5d180591d
Muhammad Sameer <sameerrafiq44@gmail.com>
2026-06-22 01:48:35 +0500

    update: v2

--- app.js
+++ app.js
@@ -1,6 +1,2 @@
-const http = require('http');
-
-http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end('Hello from Jenkins Pipeline! v1\n');
-}).listen(3000, () => console.log('Running on port 3000'));
+res.end('Hello from Jenkins Pipeline! Muhammad Sameer here
+ v2\n');
